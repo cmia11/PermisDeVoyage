720fd9023e1020baebf55bda1300b9dc9562086b
Claire Abadie <cmia11@hotmail.com>
2020-10-04 10:35:37 +0200

    mettre l'audio au début

---
+++ Permis de voyage/Assets/Script/InterfaceTimeLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+//[RequireComponent(typeof(LocalTime))]
+public class InterfaceTimeLine : MonoBehaviour
+{
+    public LocalTime localtime;
+    private AudioSource audiosource;
+    //public float value;
+    public TextMeshProUGUI horloge;
+    //GameObject gameObject = this;
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        // value = gameObject.GetComponent<Value>;
+        audiosource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
